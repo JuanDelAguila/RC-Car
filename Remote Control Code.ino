e6287106de25a6e2ebebb7f1e57a8d516a03686a
JuanDelAguila <60519613+JuanDelAguila@users.noreply.github.com>
2020-02-02 17:45:06 +0000

    Add files via upload

---
+++ Remote Control Code.ino
@@ -0,0 +1,76 @@
+
+
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+#include <EasyTransfer.h>
+EasyTransfer ET;
+struct SEND_DATA_STRUCTURE{
+  float Dir;
+  float Acc;
+  float GearS;
+};
+SEND_DATA_STRUCTURE mydata;
+int RMapa;
+int subir;
+int bajar;
+int gear = 1;
+int power;
+
+void setup() {
+  ET.begin(details(mydata), &Serial);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A7, INPUT);
+  pinMode (2, INPUT);
+  pinMode (8, INPUT);
+  pinMode (9, INPUT);
+  lcd.begin (16,2);
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  mydata.Dir = analogRead (A1);
+  mydata.Acc = analogRead (A0);
+  subir = digitalRead (8);
+  bajar = digitalRead (9);
+  if (subir == HIGH) {
+    gear = gear + 1;
+    if (gear > 6) {
+      gear = 6;
+}
+  delay (100);
+}
+  if (bajar == HIGH) {
+    gear = gear - 1;
+    if (gear < 1) {
+      gear = 1;
+}
+  delay (100);
+} 
+
+  mydata.GearS = gear;
+  ET.sendData();
+  power = map(gear, 0, 6, 0, 100);
+  lcd.setCursor(5,0);
+  lcd.print("Gear:");
+  lcd.setCursor(10,0);
+  lcd.print(gear);
+  lcd.setCursor(3,1);
+  lcd.print("Power:");
+  lcd.setCursor(9,1);
+  lcd.print(power);
+  if (power < 100){
+    lcd.setCursor(12,1);
+    lcd.print(" ");
+    lcd.setCursor(11,1);
+}
+  else {
+    lcd.setCursor(12,1);
+}
+
+  lcd.print("%");
+  delay(50);
+}
+
+
---
+++ car code.ino
@@ -0,0 +1,77 @@
+
+#include <EasyTransfer.h>
+#include <Servo.h>
+
+EasyTransfer ET;
+Servo myServo;
+Servo myServo2;
+
+int DirR;
+int AccR;
+int MapaR;
+int GearR;
+int DirRmin;
+int AccRmin;
+int DirRmax;
+int AccRmax;
+int distance;
+int duration;
+
+#define echoPin 13
+#define trigPin 12
+
+struct RECEIVE_DATA_STRUCTURE{ 
+  float Dir;
+  float Acc;
+  float GearS;
+};
+RECEIVE_DATA_STRUCTURE mydata;
+void setup() {
+  // put your setup code here, to run once:
+  ET.begin(details(mydata), &Serial);
+  myServo.attach (10);
+  myServo2.attach (11);
+  Serial.begin (9600);
+  pinMode (7, OUTPUT);
+  digitalWrite (7, HIGH);
+  pinMode (echoPin, INPUT);
+  pinMode (trigPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //if (ET.receiveData()){
+    GearR = mydata.GearS;
+    AccRmin = -13.3333 * (GearR) + 90;
+    AccRmax = 13.3333 * (GearR) + 90;
+    DirRmin = 7.5 * (GearR) + 35;
+    DirRmax = -7.5 * (GearR) + 165; 
+    digitalWrite (7, HIGH);
+    DirR = map(mydata.Dir, 0, 1023, DirRmax, DirRmin);
+    AccR = map(mydata.Acc, 0, 1023, AccRmax, AccRmin);
+    myServo.write(DirR);
+    myServo2.write(AccR);
+
+    
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds (2);
+    digitalWrite (trigPin, HIGH);
+    delayMicroseconds (10);
+    digitalWrite (trigPin, LOW);
+    duration = pulseIn (echoPin, HIGH);
+    Serial.println (distance);
+    distance = duration/58.2;
+    
+    if (distance <= 200){
+      if (distance > 0) {
+        myServo2.write(50);
+        delay (57);
+      }
+      }
+    delay (57);
+  //}
+//  else{
+//    digitalWrite (7, LOW);
+//    myServo2.write(90);
+//  }
+}
